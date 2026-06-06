a3f4d464f6ad775ba96c8153c4052341c2e84e85
Jain Chirag <jainchirag19102002@gmail.com>
2026-06-06 19:52:55 +0530

    Add files via upload

---
+++ 12.py
@@ -0,0 +1,16 @@
+# for i in range(17):
+#     if(i == 10):
+#         break
+#     print("7 X", i+1, "=", 7 * (i+1))
+    
+# for i in range(17):
+#     if(i == 10):
+#         print("Skip the iteration")
+#         continue
+#     print("7 X", i, "=", 7 *i)
+i = 0
+while True:
+    print(i)
+    i = i + 1
+    if(i%100 == 0):
+      break
