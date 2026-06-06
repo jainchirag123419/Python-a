d9656a4d2b309cf14e63fbaeb9284ef9203da88a
Jain Chirag <jainchirag19102002@gmail.com>
2026-06-06 19:46:14 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,47 @@
+print("hello world",6566,"wall")
+print(5)
+print(12*367)
+'''
+
+ Hey Harry,please dontremove this line
+ harry 
+code with harry
+'''
+print("Hey I am \'a \"good boy\"\nand this "
+      "viewer is also a good boy")
+# print("This is a print statement")
+# print("Hello world") #printing hello world
+print("Hey",6,7,sep="~", end="9\n")
+print("hello")
+print(5+6)
+print(15+6)
+print(15*6)
+print(15/6)
+print(15//6)
+print(15%6)
+print(15** 6)
+a = 56
+b = 3
+
+print("The value of",a,"+",b, "is",a+b)
+print("The value of",a,"-",b, "is",a-b)
+print("The value of",a,"*",b, "is",a*b)
+print("The value of",a,"/",b, "is",a/b)
+print("The value of",a,"%",b, "is",a%b)
+print("The value of",a,"**",b, "is",a**b)
+# a = "1"
+a = 1
+# b = "2"
+b = 2
+print(a+b)
+c= "3"
+d= "4"
+print(int(c)+int(d))
+string="12"
+number=18
+string_number = int(string)
+sum=number +string_number
+print("The Sum of both the no is",sum) 
+e=2.8
+f=5
+print(e+f)
