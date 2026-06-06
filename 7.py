46380e5064793059b40eb186bd301d97a6f6bd05
Jain Chirag <jainchirag19102002@gmail.com>
2026-06-06 19:50:30 +0530

    Add files via upload

---
+++ 7.py
@@ -0,0 +1,8 @@
+applePrice = 10
+budget = 200
+if(budget - applePrice >50):
+    print("alexa,add 1kg Apples in the cart")
+elif(budget - applePrice>70):
+    print("its okaya")
+else:
+    print("Alexa,do not add 1kg apples in the cart")
