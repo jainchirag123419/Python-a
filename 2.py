ae9e96bc1bf42ecb33cb1b0bec9223cdd4f553ca
Jain Chirag <jainchirag19102002@gmail.com>
2026-06-06 19:46:32 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,17 @@
+name ="Harry"
+friend="Rohan"
+anotherFriend='bhargav'
+apple=('''He said"
+       hi,chirag
+       hey I am good
+       I want to eat an apple''')
+print("hello,"+name)
+# print(apple)
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[1])
+for character in apple:
+    print(character)
