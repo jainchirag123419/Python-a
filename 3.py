de326540723691d9da37b0be0975bd0749f2585c
Jain Chirag <jainchirag19102002@gmail.com>
2026-06-06 19:46:55 +0530

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,19 @@
+names ="Harry,Shubhum"
+print(names[0:5])
+print(len(names))
+fruit ="Mango"
+len1 =len(fruit)
+print("Mango is a",len1,"letter word")
+fruit ="Mango"
+mangoLen =len(fruit)
+print(mangoLen)
+print(fruit[:4]) #including 0 but not 4
+print(fruit[:])
+print(fruit[1:4])
+print(fruit[0:-3])
+print(fruit[0:len(fruit[-3])])
+print(fruit[0:len(fruit)-3])
+print(fruit[-1:len(fruit)-3])
+print(fruit[-3:-1])
+nm = "Harry"
+print(nm[-4:-2])
