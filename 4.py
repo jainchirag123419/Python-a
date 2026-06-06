f5d5eedad3f4b116d029ef0bcd0f461dd4b6099d
Jain Chirag <jainchirag19102002@gmail.com>
2026-06-06 19:48:03 +0530

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,45 @@
+a="!!!Harry!! !!!!!!!! Harry!!"
+print(len(a))
+print(a.upper())
+print(a.lower())
+print(a.rstrip("!"))
+print(a.replace("Harry","John"))
+print(a.split(" "))
+blogHeading = "introduction to jS"
+print(blogHeading)
+str1 = "Welcome to console!!!"
+print(len(str1))
+print(len(str1.center(50)))
+print(a.count("Harry"))
+str1 = "Welcome to console!!!"
+print(str1.endswith("!!"))
+str1 = "Welcome to console!!!"
+print(str1.endswith("to",4,10))
+str1 = "He's name is Dan. He is a teacher"
+print(str1.find("ishh"))
+# print(str1.index("ishh"))
+str1 = "Welcometoconsole"
+print(str1.isalnum())
+str1 = "Welcome"
+print(str1.isalpha())
+str1 = "Welcome00"
+print(str1.isalpha())
+str1 = "welcome"
+print(str1.islower())
+str1="We  wish you a merry christmas\n"
+print(str1)
+print(str1.isprintable())
+str1="    "  
+print(str1.isspace())
+str2 ="world health organization"
+print(str2.istitle())
+str2 ="World Health Organization"
+print(str2.istitle())
+str1="Python is a interpreted language"
+print(str1.startswith("Python"))
+str1="Python is a interpreted language"
+print(str1.startswith("Pythona"))
+str1="Python is a interpreted language"
+print(str1.swapcase())
+str1="Python is a interpreted language"
+print(str1.endswith("language"))
