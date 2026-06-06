6a409e83f7344c5ece76edc6a53627b4974f7311
Jain Chirag <jainchirag19102002@gmail.com>
2026-06-06 19:48:53 +0530

    Add files via upload

---
+++ 5.py
@@ -0,0 +1,13 @@
+a = int(input("Enter your age"))
+print("your age is",a)
+# print(a>18)
+# print(a<=18)
+# print(a==18)
+# print(a!=18)
+if(a>18):
+    print("you can drive")
+    print("yes")
+else:
+    print("you can't drive")
+    print("no")
+print("yay!")
