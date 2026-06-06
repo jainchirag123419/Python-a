6d4b023bf50bf6c779bb7e3d637384a6910e4637
Jain Chirag <jainchirag19102002@gmail.com>
2026-06-06 19:51:01 +0530

    Add files via upload

---
+++ 8.py
@@ -0,0 +1,10 @@
+num = int(input("Enter the Value"))
+if (num > 0):
+    print("number is positive")
+elif (num == 0):
+    print("number is zero")
+elif (num == 999):
+    print("number is special")
+else:
+    print("number is negative")
+print("I am happy now")
